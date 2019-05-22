1ec404ac1df3f3c3f30bb9443503d832e0089ecc
Cleve716 <cleve716@msn.com>
2019-05-22 14:13:56 -0600

    axios omdb added

--- keys.js
+++ keys.js
@@ -1,7 +1,7 @@
 console.log('this is loaded');
 
 exports.spotify = {
-    id: process.env.c35902e9deae47b2adb1bee8fd624958,
-    secret: process.env.4f1e0a5d53964981adb23a2da1e64b5a
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET,
   };
   
--- liri.js
+++ liri.js
@@ -1,1 +1,19 @@
 require("dotenv").config();
+//var keys = require("./keys.js");
+//var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+//var = getMovieData;
+
+axios.get("http://www.omdbapi.com/?t=fuel&plot=short&apikey=trilogy").then(
+    function (response) {
+        console.log("Title: " + response.data.Title);
+        console.log("Year released: " + response.data.Year);
+        console.log("IMDB rating: " + response.data.imdbRating);
+        console.log("Rotten Tomatoes rating: " + response.data.Ratings[1].Value);
+        console.log("Produced in: " + response.data.Country);
+        console.log("Language: " + response.data.Language);
+        console.log("Plot: " + response.data.Plot);
+        console.log("Actors: " + response.data.Actors);
+
+    }
+);
